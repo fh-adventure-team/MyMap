76fae4f3c606a21d33da45bcb004d39b705c74fd
Helen Hinrichs <59894635+lady-lazy@users.noreply.github.com>
2021-02-18 14:55:07 +0100

    Delete mapUtilities.tsx

--- mapUtilities.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="../../world-map-starterkit-master/world-map-starterkit-master/tiles_png/structures/mapUtilities.png" width="288" height="32"/>
-</tileset>
